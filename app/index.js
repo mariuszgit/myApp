934614a40e9e07a759df9a134498b6e4d6cf28c7
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2024-03-26 21:59:56 +0100

    correction

---
+++ app/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+
+// app.set('view engine', 'hbs')
+
+// app.get('/', (req, res) => {
+//     // res.send('Strona główna')
+//     res.render('index', {
+//         pageTitle: 'xxx'
+//     })
+// })
+
+app.get('/contact', (req, res) => {
+    res.send('Strona konaktowa')
+})
+
+app.listen(port, () => {
+    console.log("Listening to port nr " + port + '.');
+})
