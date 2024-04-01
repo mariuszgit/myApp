1b5286ebe2e9d4174876e13ce05b828b4b858692
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2024-04-01 23:42:42 +0200

    add socket-io

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
+const { io } = require('socket.io')(port)
 
 // app.set('view engine', 'hbs')
+
+io.on('connection', socket => {
+    console.log('connected to socket');
+    console.log(socket.id);
+})
 
 app.get('/', (req, res) => {
     res.send('Strona główna')
